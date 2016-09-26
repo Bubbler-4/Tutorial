89f1d55cfa38d946c41bb2bb2bdf26aac744a41f
Bubbler-4 <bubbler9903@gmail.com>
2016-09-26 22:00:26 +0900

    Finish awesome feature

--- Tutorial/Tutorial.cpp
+++ Tutorial/Tutorial.cpp
@@ -5,10 +5,15 @@ void awesomeFeature() {
 	cout << "This is AWESOME!!" << endl;
 }
 
+void awesomeFeature2(int n) {
+	cout << "This is " << n << " times AWESOME!!!!" << endl;
+}
+
 int main() {
 	cout << "This is NOT a Tutorial!" << endl;
 
 	awesomeFeature();
+	awesomeFeature2(3);
 
 	return 0;
 }
