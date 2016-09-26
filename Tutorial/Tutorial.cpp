4a44f03f15582820b9e04e151962405ca00e0984
Bubbler-4 <bubbler9903@gmail.com>
2016-09-26 21:33:08 +0900

    Add an awesome feature

--- Tutorial/Tutorial.cpp
+++ Tutorial/Tutorial.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void awesomeFeature() {
+	cout << "This is AWESOME!!" << endl;
+}
+
 int main() {
 	cout << "This is a Tutorial!" << endl;
+
+	awesomeFeature();
 
 	return 0;
 }
